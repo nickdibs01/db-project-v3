1c19e40bfdf3e2fe7fbf5292df0bc9ffd95ba24c
nickdibs01 <120411499+nickdibs01@users.noreply.github.com>
2024-03-27 00:54:41 +0000

    Version 1 for project report 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,125 +1,77 @@
 import streamlit as st 
 import pandas as pd
-
-st.balloons()
-st.markdown("# Data Evaluation App")
-
-st.write("We are so glad to see you here. ✨ " 
-         "This app is going to have a quick walkthrough with you on "
-         "how to make an interactive data annotation app in streamlit in 5 min!")
-
-st.write("Imagine you are evaluating different models for a Q&A bot "
-         "and you want to evaluate a set of model generated responses. "
-        "You have collected some user data. "
-         "Here is a sample question and response set.")
-
-data = {
-    "Questions": 
-        ["Who invented the internet?"
-        , "What causes the Northern Lights?"
-        , "Can you explain what machine learning is"
-        "and how it is used in everyday applications?"
-        , "How do penguins fly?"
-    ],           
-    "Answers": 
-        ["The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting" 
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds."
-    ]
-}
-
-df = pd.DataFrame(data)
-
-st.write(df)
-
-st.write("Now I want to evaluate the responses from my model. "
-         "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-         "You will now notice our dataframe is in the editing mode and try to "
-         "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇")
-
-df["Issue"] = [True, True, True, False]
-df['Category'] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config = {
-        "Questions":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Answers":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Issue":st.column_config.CheckboxColumn(
-            "Mark as annotated?",
-            default = False
-        ),
-        "Category":st.column_config.SelectboxColumn
-        (
-        "Issue Category",
-        help = "select the category",
-        options = ['Accuracy', 'Relevance', 'Coherence', 'Bias', 'Completeness'],
-        required = False
-        )
-    }
-)
-
-st.write("You will notice that we changed our dataframe and added new data. "
-         "Now it is time to visualize what we have annotated!")
-
-st.divider()
-
-st.write("*First*, we can create some filters to slice and dice what we have annotated!")
-
-col1, col2 = st.columns([1,1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options = new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox("Choose a category", options  = new_df[new_df["Issue"]==issue_filter].Category.unique())
-
-st.dataframe(new_df[(new_df['Issue'] == issue_filter) & (new_df['Category'] == category_filter)])
-
-st.markdown("")
-st.write("*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`")
-
-issue_cnt = len(new_df[new_df['Issue']==True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
-
-col1, col2 = st.columns([1,1])
-with col1:
-    st.metric("Number of responses",issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
-
-df_plot = new_df[new_df['Category']!=''].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x = 'Category', y = 'count')
-
-st.write("Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:")
-
-
-
-
-
 import streamlit as st
+
+st.set_page_config(layout="wide")
+
+
+st.title("Delirium and Sepsis in Transgenic Mice Database")
 
 # Initialize connection.
 conn = st.connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * FROM Mice;', ttl=0)
+# Perform queries.
+#DataRepository = conn.query('SELECT * FROM DataRepository;', ttl=1)
+#Intervention = conn.query('SELECT * FROM Intervention;', ttl=1)
+#Mice = conn.query('SELECT * FROM Mice;', ttl=1)
+Sequencing = conn.query('SELECT * FROM Sequencing;', ttl=1)
+Study = conn.query('SELECT * FROM Study;', ttl=1)
+
 
 # Print results.
-for row in df.itertuples():
-    st.write(row)
+#for row in Mice.itertuples():
+#    st.write(row)
+
+
+
+col1, col2 = st.columns([1,1])
+with col1:
+    disease_filter = st.multiselect("Disease Focus", options = conn.query("SELECT DISTINCT DiseaseFocus FROM Study;", ttl=1), 
+                                    default=None)
+with col2:
+    name_filter = st.text_input("Sequencing Platform", placeholder="Enter Search")
+
+select1 = ("SELECT Title,DiseaseFocus,Platform,DOI,URL,Journal,DatePub,ContactName,ContactEmail "
+            "FROM Study AS st JOIN Sequencing AS seq ON seq.StudyID = st.StudyID WHERE ")
+
+if disease_filter != []:
+    for i in range(len(disease_filter)):
+        if i == 0:
+            select1 += f"(DiseaseFocus='{disease_filter[i]}'"
+        else:
+            select1 += f" OR DiseaseFocus='{disease_filter[i]}'"
+    select1 += f") AND Platform LIKE '%{name_filter}%';"
+else:
+    select1 += f"Platform LIKE '%{name_filter}%';"
+
+
+st.dataframe(conn.query(select1, ttl=1))
+
+st.divider()
+
+
+table_name = st.selectbox('Table', options=conn.query("""SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES 
+                                                WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='MiceDB';"""))
+
+select2 = select1.replace("SELECT ", "SELECT st.StudyID,")
+
+df = conn.query(select2)
+
+IDs = df[df.columns[0]]
+
+
+
+
+filter = st.toggle("Use above filters")
+
+if filter == False:
+    st.dataframe(conn.query(f"Select * FROM {table_name};"))
+else:
+    select3 = "SELECT * FROM"
+    
+    for i in IDs:
+        
+        st.dataframe()
+
+
+
